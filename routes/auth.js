0063ae7f7d194778b0d98ffb8f18acae71bdba20
Mayank Vats <mv@rapidinnovation.dev>
2021-02-03 18:37:01 +0530

    add Joi validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+const { registerValidation, loginValidation } = require('../validation');
+
 router.post('/register', async (req, res) => {
+
+    // Validate the data
+    const { error } = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    // Check if user is already in DB
+    const emailExist = await User.findOne({ email: req.body.email });
+    if(emailExist) return res.status(400).send('Email already exists');
+
+    // Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
