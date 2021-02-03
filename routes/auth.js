36bac46d26988def425a572383c560b70dc65e54
Mayank Vats <mv@rapidinnovation.dev>
2021-02-03 19:20:35 +0530

    apply password hash

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
-
+const bcrypt = require('bcrypt');
 const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
@@ -13,21 +13,39 @@ router.post('/register', async (req, res) => {
     const emailExist = await User.findOne({ email: req.body.email });
     if(emailExist) return res.status(400).send('Email already exists');
 
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     // Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
         const saveUser = await user.save();
-        res.send(saveUser);
+        res.send({ user: user._id });
     } catch(err) {
         res.status(400).send(err);
     }
 });
 
-// router.post('/login')
+router.post('/login', async (req, res) => {
+    // Validate the data
+    const { error } = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    
+    // Check if user is already in DB
+    const user = await User.findOne({ email: req.body.email });
+    if(!user) return res.status(400).send('Email or Password is wrong');
+
+    // Check if password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send("Invalid Password");
+
+    res.send("Logged In");
+});
 
 module.exports = router;
