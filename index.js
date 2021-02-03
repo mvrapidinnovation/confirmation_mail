15c72d93544778b23f8bc738589f23264ba12f23
Mayank Vats <mv@rapidinnovation.dev>
2021-02-03 17:37:48 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// IMPORT ROUTES
+const authRoute = require('./routes/auth');
+
+const logger = function(req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+}
+
+dotenv.config();
+
+// CONNECT TO DB
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(process.env.MONGO_URL, options, () => {
+    console.log('Connected to DB!')
+});
+
+// Middlewares
+app.use(logger);
+app.use(express.json());
+
+// ROUTES MIDDLEWARE
+app.use('/api/user', authRoute);
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
