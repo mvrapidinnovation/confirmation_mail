8887b17f382ef063d29284b4d3c2a1ba210b5650
Mayank Vats <mv@rapidinnovation.dev>
2021-02-03 19:50:39 +0530

    apply JWT authorization

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 
 // IMPORT ROUTES
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/post');
 
 const logger = function(req, res, next) {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
@@ -25,5 +26,6 @@ app.use(express.json());
 
 // ROUTES MIDDLEWARE
 app.use('/api/user', authRoute);
+app.use('/api/post', postRoute);
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const { registerValidation, loginValidation } = require('../validation');
 
@@ -45,7 +46,9 @@ router.post('/login', async (req, res) => {
     const validPass = await bcrypt.compare(req.body.password, user.password);
     if(!validPass) return res.status(400).send("Invalid Password");
 
-    res.send("Logged In");
+    // Create and sign a token
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
 });
 
 module.exports = router;
